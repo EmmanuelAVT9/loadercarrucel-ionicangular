c7a6ec0bb81323bdfbf1af6a4ebe2df663355d37
EmmanuelAVT9 <alexsoge@gmail.com>
2024-10-24 13:39:44 -0600

    feat: Add module in Tab1 and import component FullScreenLoader

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,40 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FullScreenLoaderComponent } from '../components/full-screen-loader/full-screen-loader.component';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page implements OnInit {
-  progress: number = 0; // Inicializamos el progreso en 0
-  images: string[] = ['assets/img1.jpg', 'assets/img2.jpg', 'assets/img3.jpg', 'assets/img4.jpg'];
-  currentImage: string = this.images[0]; 
-  currentIndex: number = 0;
+export class Tab1Page{
+
+  // Definimos la referencia al componente del loader
+  @ViewChild(FullScreenLoaderComponent) loader!: FullScreenLoaderComponent;
+  
+  loaderImages = [
+    { src: 'assets/img1.jpg', text: 'Cargando recursos ...' },
+    { src: 'assets/img2.jpg', text: 'Dame un momento estamos trabajando para darte una mayor experiencia...' },
+    { src: 'assets/img3.jpg', text: 'Ya casi esta todo listo espera...' },
+    { src: 'assets/img4.jpg', text: 'Finalizando carga...' }
+  ];
 
   constructor() {}
 
-  ngOnInit() {
-    this.startProgress();
-  }
-
-  startProgress() {
-    const totalDuration = 8000; // Duración total de la animación (8 segundos)
-    const intervalTime = 80; // Intervalo para avanzar el progreso (cada 80ms)
-    const increment = 100 / (totalDuration / intervalTime); // Incremento en cada intervalo
-
-    const interval = setInterval(() => {
-      if (this.progress >= 100) {
-        clearInterval(interval); // Detenemos cuando llega al 100%
-      } else {
-        this.progress += increment;
-
-        // Cambia de imagen en los porcentajes 25%, 50%, 75%, 100%
-        const newIndex = Math.min(Math.floor(this.progress / 25), this.images.length - 1);
-        if (newIndex !== this.currentIndex) {
-          this.currentIndex = newIndex;
-          this.currentImage = this.images[this.currentIndex];
-        }
-      }
-    }, intervalTime);
+  showLoader() {
+    if (this.loader) {
+      this.loader.startLoader(); // Llamamos al método startLoader del componente hijo
+    }
   }
 }
