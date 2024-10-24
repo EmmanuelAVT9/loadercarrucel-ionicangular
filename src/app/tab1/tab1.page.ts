796c017e9fd78f98040b88179fd2dc41ecceb7a5
EmmanuelAVT9 <alexsoge@gmail.com>
2024-10-24 11:12:07 -0600

    feat: Create carrucel, progress bar and text percentage, styles and functionality

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  progress: number = 0; // Inicializamos el progreso en 0
+  images: string[] = ['assets/img1.jpg', 'assets/img2.jpg', 'assets/img3.jpg', 'assets/img4.jpg'];
+  currentImage: string = this.images[0]; 
+  currentIndex: number = 0;
 
   constructor() {}
 
+  ngOnInit() {
+    this.startProgress();
+  }
+
+  startProgress() {
+    const totalDuration = 8000; // Duración total de la animación (8 segundos)
+    const intervalTime = 80; // Intervalo para avanzar el progreso (cada 80ms)
+    const increment = 100 / (totalDuration / intervalTime); // Incremento en cada intervalo
+
+    const interval = setInterval(() => {
+      if (this.progress >= 100) {
+        clearInterval(interval); // Detenemos cuando llega al 100%
+      } else {
+        this.progress += increment;
+
+        // Cambia de imagen en los porcentajes 25%, 50%, 75%, 100%
+        const newIndex = Math.min(Math.floor(this.progress / 25), this.images.length - 1);
+        if (newIndex !== this.currentIndex) {
+          this.currentIndex = newIndex;
+          this.currentImage = this.images[this.currentIndex];
+        }
+      }
+    }, intervalTime);
+  }
 }
