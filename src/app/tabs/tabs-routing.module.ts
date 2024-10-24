204b6d1cd0d18cb1cfcc3c53773bc624cd625f42
EmmanuelAVT9 <alexsoge@gmail.com>
2024-10-24 13:40:17 -0600

    feat: Add route in tabs for path tab 3 redirect

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
+import { Tab3Page } from '../tab3/tab3.page';
 
 const routes: Routes = [
   {
@@ -30,7 +31,8 @@ const routes: Routes = [
     path: '',
     redirectTo: '/tabs/tab1',
     pathMatch: 'full'
-  }
+  },
+  { path: 'tab3', component: Tab3Page },
 ];
 
 @NgModule({
